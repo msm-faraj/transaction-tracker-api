e901d3852fddcc3483d815720e8a785c7f2fd04d
msm-faraj <farajzademeysam@gmail.com>
2024-03-15 13:51:16 +0100

    delete getone method from account

--- routes/accounts.js
+++ routes/accounts.js
@@ -12,7 +12,6 @@ const reqHandler = require("../middleware/req-handel");
 router.post("/", reqHandler(controller.create.bind(controller)));
 router.put("/:id", reqHandler(controller.update.bind(controller)));
 router.delete("/:id", reqHandler(controller.delete.bind(controller)));
-router.get("/:id", reqHandler(controller.getOne.bind(controller)));
 router.get("/", reqHandler(controller.getAll.bind(controller)));
 
 module.exports = router;
