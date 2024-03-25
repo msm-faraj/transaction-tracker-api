5afa471b69bb27284a92e6c3e36991b2a7824af5
msm-faraj <farajzademeysam@gmail.com>
2024-03-25 08:53:34 +0100

    setting association of users and accounts

---
+++ migrations/20240325074842-edit-accounts-userId-column.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    //Change userId column of accounts to set association with user
+    await queryInterface.changeColumn("Accounts", "userId", {
+      type: Sequelize.UUID,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("Accounts", "userId", {
+      type: Sequelize.UUID,
+    });
+  },
+};
--- models/account.js
+++ models/account.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Account.belongsTo(models.User, {
+        as: "user",
+        foreignKey: "userId",
+      });
     }
   }
   Account.init(
